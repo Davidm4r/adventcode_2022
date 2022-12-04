bf7a9ca3b10fce9783401fb147bfd23bb0d693c4
David Martin Bragado <david.martin@david.martin@eventbrite.com-20636>
2022-12-04 15:25:56 +0100

    Day 04

---
+++ day_04/02.py
@@ -0,0 +1,21 @@
+filepath = 'input2.txt'
+import re
+with open(filepath) as fd:
+    total_sum_priority = 0
+    lines = fd.read().splitlines()
+    total_points = 0
+    count = 0
+    for line in lines:
+        assigments = re.findall(r'\d+', line)
+        number1_first = int(assigments[0])
+        number2_first = int(assigments[1])
+        number1_second = int(assigments[2])
+        number2_second = int(assigments[3])
+
+        list_1 = [x for x in range(number1_first,number2_first+1)]
+        list_2 = [x for x in range(number1_second, number2_second+1)]
+
+        list1_contains_list2 = set(list_1).intersection(set(list_2))        
+        if list1_contains_list2:
+            count+=1
+    print(count)
